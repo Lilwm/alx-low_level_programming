9a035646d0cb3fb4b5a5176fdf759306720dba5d
Lilwm <lmiiri95@gmail.com>
2022-03-12 09:09:41 +0000

    initial commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*0-9 task
+*Return: return 0 after program executio
+*/
+int main(void)
+{
+/*definition of variable*/
+int i = 0;
+{
+/*beggining of While loop to print all single digits*/
+while (i < 10)
+{
+printf("%d\n", i);
+i++;
+}
+return (0);
+}
+}
