6279a325337230416813c9a0644fd90eafb3aa9c
Lilwm <lmiiri95@gmail.com>
2022-03-13 07:16:41 +0000

    correct naming of the file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *Main -Entry point
+ *My Hello printf task
+ *Return: return 1 after program execution
+ */
+int main (void)
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
