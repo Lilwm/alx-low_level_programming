53aa96cfbfbaec3c9ceb774c0fd6dcbc54336791
Lilwm <lmiiri95@gmail.com>
2022-03-15 15:58:52 +0000

    conformed to betty coding style

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,9 @@
 #include "main.h"
-/*
- * int _isalpha - shows if a char is an alphabet
+/**
+ * _isalpha - Checks if a character is alphabetic.
+ * @c: The character to be checked.
  *
- * return: 1 if c is an alphabet else, 0
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
  */
 
 int _isalpha(int c)
