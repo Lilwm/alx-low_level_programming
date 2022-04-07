be3be5633a62c339f8453d7fcf4cfcbf81e3af10
Lilwm <lmiiri95@gmail.com>
2022-04-07 15:01:56 +0000

    fixed output issue

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,10 +36,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		array[i] = s1[i];
 
 	for (j = 0; j < s2_len; j++)
-		array[j + s1_len] = s2[j];
+	{
+		array[i] = s2[j];
+		i++;
+	}
+	i++;
 
-
-	array[j + s1_len] = '\0';
+	array[i] = '\0';
 
 	return (array);
 }
