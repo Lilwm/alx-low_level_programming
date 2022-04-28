6a92d27b171df43a452fceddd359c61acdacc542
Lilwm <lmiiri95@gmail.com>
2022-04-28 11:38:02 +0000

    sorted issue of adding 2 strings

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <string.h>
+
 /**
  * add_node_end - Adds a new node at the end
  *                of a list_t list.
@@ -9,43 +10,40 @@
  * Return: If the function fails - NULL.
  *         Otherwise - the address of the new element.
  */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
-	int len;
 	char *dup;
-	list_t *new_node;
-	list_t *current_node;
+	int len;
+	list_t *new, *last;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-	{
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
-	}
+
 	dup = strdup(str);
 	if (str == NULL)
 	{
-		free(new_node);
+		free(new);
 		return (NULL);
 	}
 
-	for (len = 0; str[len]; len++)
-	new_node->str = dup;
-	new_node->len = len;
-	new_node->next = NULL;
+	for (len = 0; str[len];)
+		len++;
 
-	if (*head == NULL)      /*check for empty linked list*/
-		*head = new_node;
+	new->str = dup;
+	new->len = len;
+	new->next = NULL;
 
-	current_node = *head;
+	if (*head == NULL)
+		*head = new;
 
-	while (current_node->next != NULL)
+	else
 	{
-		current_node = current_node->next;
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new;
 	}
-	current_node->next = new_node;
-	new_node->next = NULL;
 
 	return (*head);
 }
-
